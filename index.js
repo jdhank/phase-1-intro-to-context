d62b3c6aff5a3ee6c9db738fd680afbdfd6b8338
Jake Hankins <jakehankins@comcast.net>
2022-05-17 13:03:00 -0400

    commit tree b918e9dc9569fdb637c1d63c8c2975bf5292f566

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName : recArray[0],
+        familyName : recArray[1],
+        title: recArray[2],
+        payPerHour : recArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+}
+function createTimeInEvent(event){
+    let [date,hour] = event.split(" ")
+    let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    }
+    employee.timeInEvent.push(EventObj)
+    
+}
